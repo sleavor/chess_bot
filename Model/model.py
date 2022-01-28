bf782049133a8781dc26c90e7d93a97adf8e9a69
Shawn Leavor <sleavor@yahoo.com>
2022-01-27 21:05:54 -0600

    Open data files

--- Model/model.py
+++ Model/model.py
@@ -1,4 +1,18 @@
-import requests
+#Import libraries
+import chess.pgn
+import os
 
-response = requests.get("https://explorer.lichess.ovh/masters/")
-print(response.text)
+#Open data file Carlsen.pgn
+cur_path = os.path.dirname(__file__)
+# EDIT PATH TO YOUR DATA #
+data_path = '../Data/Carlsen.pgn'
+path=os.path.join(cur_path, data_path)
+path=os.path.abspath(path)
+data = open(path)
+
+while True:
+    try:
+        game = chess.pgn.read_game(data)
+        print(game.headers["Event"])
+    except AttributeError:
+        break
